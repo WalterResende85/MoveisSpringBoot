dd72c5fb30c8df815f71daf7f1928f14f548d63b
WalterResende85 <walterccr@hotmail.com>
2019-10-19 22:48:35 -0300

    algumas alteracoes

--- src/main/java/com/app/moveis/controller/ClienteController.java
+++ src/main/java/com/app/moveis/controller/ClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.app.moveis.model.Cliente;
 import com.app.moveis.repository.ClienteRepository;
@@ -13,14 +14,19 @@ public class ClienteController {
 	@Autowired
 	ClienteRepository cr;
 	
-	@RequestMapping("/cadastroCliente")
+
+	@RequestMapping(value="/cadastroCliente", method = RequestMethod.GET)
 	public String cadastroCliente() {
 		
 		return "/cadastroCliente";
 	}
+	
 	@RequestMapping(value="/cadastroCliente", method = RequestMethod.POST)
 	public void cadastrarCliente(Cliente cliente) {
 		cr.save(cliente);
 	}
+	
+	
+	
 	
 }
--- src/main/java/com/app/moveis/controller/IndexController.java
+++ src/main/java/com/app/moveis/controller/IndexController.java
@@ -1,19 +1,35 @@
 package com.app.moveis.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.app.moveis.model.Cliente;
+import com.app.moveis.repository.ClienteRepository;
 
 @Controller
 public class IndexController {
-
+	@Autowired
+	ClienteRepository cr;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
-	@RequestMapping("/listaCliente")
+	@RequestMapping(value="/manterCliente", method = RequestMethod.GET)
 	public String listaCliente() {
 		return "listaCliente";
 	}
+	@RequestMapping("/manterCliente")
+	public ModelAndView listaClientes() {
+		ModelAndView mv = new ModelAndView("listaCliente");
+		Iterable<Cliente> clientes = cr.findAll();
+		mv.addObject("clientes", clientes);
+		return mv;
+	}
+	
 	@RequestMapping("/listaFuncionario")
 	public String listaFuncionario() {
 		return "listaFuncionario";
